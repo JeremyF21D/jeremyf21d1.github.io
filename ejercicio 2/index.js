31853035e5bd9cf643086ed1499ec2a4d2970cd9
JeremyF21D <107584598+JeremyF21D@users.noreply.github.com>
2023-02-16 16:27:17 -0400

    Add files via upload

---
+++ ejercicio 2/index.js
@@ -0,0 +1,14 @@
+
+capitali = 250.00;
+interesa = 15;
+interesT = 0;
+capitalF = 0;
+capitalN = 0;
+
+
+capitalF =  capitali * 12;
+interesT = capitalF * interesa / 100;
+capitalN = capitalF + interesT;
+console.log("La capital final es: " + capitalN + " bs");
+console.log("El interes anual es: " + interesa+ " %");
+
